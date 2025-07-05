a69455dae34f0917663fb91a1fb5a65a8d616064
Simran-Kumari92 <simranku20@gmail.com>
2025-07-05 15:41:19 +0530

    onBoarding Page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,25 +13,17 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 // Toast notifications
 import { Toaster, toast } from "react-hot-toast";
 
-// React Query: handles API data fetching and caching
-import { useQuery } from "@tanstack/react-query";
 
-// Axios for API calls
-import axios from "axios";
-import { axiosInstance } from "./lib/axios.js";
+import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 
 const App = () => {
-  // React Query to fetch current logged-in user's info
-  const { data:authData, isLoading, error } = useQuery({
-    queryKey: ["authUser"], // unique key for caching
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false, // auth check
-  });
+  const { isLoading, authUser } = useAuthUser();
 
-  const authUser = authData?.user;
+  const isAuthenticated = Boolean(authUser)
+  const isOnboarded = authUser?.isOnboarded
+
+  if (isLoading) return <PageLoader />
 
   return (
     // Full screen container with night theme (likely using DaisyUI or custom CSS)
@@ -39,13 +31,29 @@ const App = () => {
 
       {/* All frontend routes are declared here */}
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
-        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
-        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+        <Route path="/" element={isAuthenticated && isOnboarded ? (
+          <HomePage />
+        ) : (
+          <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )
+      } 
+    />
+        <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/notifications" element={isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/onboarding" element=   {isAuthenticated ? ( 
+          !isOnboarded ? (
+            <OnboardingPage />
+          ) : ( 
+            <Navigate to="/" />
+          )
+        ) : (
+          <Navigate to="/login" />
+        )
+        }
+      />
       </Routes>
 
       {/* Toast container to display success/error messages globally */}
