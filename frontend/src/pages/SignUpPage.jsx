c681002bb502f14b8dc37b7bfc0f67802b15f6e0
Simran-Kumari92 <simranku20@gmail.com>
2025-07-05 20:14:48 +0530

    LoginPage Done

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -177,6 +177,7 @@ import { ShipWheelIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signup } from "../lib/api";
+import useSignUp from "../hooks/useSignUp";
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
@@ -184,17 +185,20 @@ const SignUpPage = () => {
     email: "",
     password: "",
   });
-
-  const queryClient = useQueryClient();
   
-  const { 
-    mutate:signupMutation, 
-    isPending, 
-    error, 
-  } = useMutation({
-    mutationFn: signup,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
-  });
+  // This is how we did it first version, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const { 
+  //   mutate:signupMutation, 
+  //   isPending, 
+  //   error, 
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  // This is how we did it using our custom hook - optimized version
+  const {isPending, error, signupMutation} = useSignUp();
 
 
   const handleSignup = (e) => {
